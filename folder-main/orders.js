a167a2cc6fc7495a6ac6db3b57d625a0b3aff4b8
pot030321 <quanganhzzz8@gmail.com>
2023-10-15 21:49:50 +0700

    web

---
+++ folder-main/orders.js
@@ -0,0 +1,20 @@
+const Orders=[
+    {
+        productName: 'clother cold',
+        productNumber: '249226',
+        PaymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'clother hot',
+        productNumber: '249456',
+        PaymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'clother coldhot',
+        productNumber: '249416',
+        PaymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
